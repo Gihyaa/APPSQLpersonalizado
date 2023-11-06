d0daab4e303bc5774c17244b0ca3f17b2f60960d
Gihyaa <giovannaap.vivencio@gmail.com>
2023-11-06 13:47:14 -0300

    SQL

---
+++ App-BD-1-master/App-BD-1-master/app/src/main/java/com/example/app_bd_1/model/CourseModel/CourseModel.kt
@@ -0,0 +1,9 @@
+package com.example.app_bd_1.model.CourseModel
+
+data class CourseModel(
+    // on below line we are creating variables for name and job
+    var courseName: String,
+    var courseDuration: String,
+    var courseTracks: String,
+    var courseDescription: String
+)
